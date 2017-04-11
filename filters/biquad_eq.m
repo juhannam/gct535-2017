ca7b8f3c592a77a091c7c8307d3c42d83500ae94
Juhan Nam <juhan.nam@gmail.com>
2017-04-11 23:25:48 +0900

    correct EQ coefficients again

--- filters/biquad_eq.m
+++ filters/biquad_eq.m
@@ -1,4 +1,4 @@
-function y = biquad_eq(x, fc, fs, Q, AdB)
+function [y, B, A] = biquad_eq(x, fc, fs, Q, GdB)
 
 % biquad_eq.m
 %
@@ -16,25 +16,25 @@ function y = biquad_eq(x, fc, fs, Q, AdB)
 
 %
 % cut-off freq: fc (Hz)
-% AdB: gain (dB)
+% GdB: gain (dB)
 % bandwidth: Q 
 % sampling rate: fs (Hz)
 %
 
 w0 = 2*pi*fc/fs;
 
-G = sqrt( 10^(AdB/20) );
+A = sqrt( 10^(GdB/20) );
 w0 = 2*pi*fc/fs;
 alpha = sin(w0)/2/Q;
 
-b0 = 1 + alpha*G;
+b0 = 1 + alpha*A;
 b1 = -2*cos(w0);
-b2 = 1 - alpha*G;
+b2 = 1 - alpha*A;
 
 alpha = sin(w0)/2/Q;
-a0 = 1 + alpha/G;
+a0 = 1 + alpha/A;
 a1 = -2*cos(w0);
-a2 = 1 - alpha/G;
+a2 = 1 - alpha/A;
 
 B = [b0 b1 b2];
 A = [a0 a1 a2];
