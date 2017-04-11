42d09500d3596ee731a0f43bc033db9219a8b9a7
Juhan Nam <juhan.nam@gmail.com>
2017-04-11 20:36:35 +0900

    correct EQ coefficients

--- filters/biquad_eq.m
+++ filters/biquad_eq.m
@@ -32,9 +32,9 @@ b1 = -2*cos(w0);
 b2 = 1 - alpha*G;
 
 alpha = sin(w0)/2/Q;
-a0 = 1 + alpha;
+a0 = 1 + alpha/G;
 a1 = -2*cos(w0);
-a2 = 1 - alpha;
+a2 = 1 - alpha/G;
 
 B = [b0 b1 b2];
 A = [a0 a1 a2];
