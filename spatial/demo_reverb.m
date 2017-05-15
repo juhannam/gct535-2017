12a4d9a768bedbb066453de31d1d08dfec53ff03
Juhan Nam <juhan.nam@gmail.com>
2017-05-15 12:55:23 +0900

    add convolution reverb

---
+++ spatial/demo_reverb.m
@@ -0,0 +1,20 @@
+% input
+[x,fs] = audioread('cello.wav');
+
+%% IR
+[h,fs2] = audioread('memchu_ir.wav');
+
+plot(h, 'LineWidth',1);
+set(gca,'FontSize', 15);
+xlim([1 20000]);
+%ylim([-0.8 0.8]);
+title('Room Impulse Response');
+
+tic
+y1 = conv(x(:,1),h);
+y2 = conv(x(:,2),h);
+toc
+
+y = [y1 y2];
+
+soundsc(y,fs);
